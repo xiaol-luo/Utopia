cd5a04a8ba3febb3c9b080257df85996b817e7ef
xiaol-luo <xiaol.luo@163.com>
2017-08-29 20:51:54 +0800

    modify

---
+++ Code/Client/Assets/Scripts/TryNetwork.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class TryNetwork : MonoBehaviour {
+
+    public string host = "127.0.0.1";
+    public int port = 10240;
+
+    Socket socket = null;
+
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (null != socket)
+                socket.Close();
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipe);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (null != socket)
+                socket.Close();
+        }
+    }
+}
--- Tools/GenProtobuf/gen.py
+++ Tools/GenProtobuf/gen.py
@@ -1,6 +1,18 @@
 import os
+import sys
 import subprocess
 import shlex
 
-subprocess.call(
-    shlex.split("protoc.exe --proto_path=./proto  --cpp_out=./cpp_protobuf msg.proto test.proto"), shell=True)
+proto_files = [
+	"msg.proto",
+	"test.proto"
+]
+proto_files_str = " ".join(proto_files)
+cpp_out = "--cpp_out=./cpp_code"
+python_out = "--python_out=./python_code"
+csharp_out = "--csharp_out=./csharp_code"
+cmd_format = "protoc.exe --proto_path=./proto {0} {1}"
+
+subprocess.call(shlex.split(cmd_format.format(cpp_out, proto_files_str)), shell=True)
+subprocess.call(shlex.split(cmd_format.format(python_out, proto_files_str)), shell=True)
+ 
