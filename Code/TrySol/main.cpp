5058209af4037c4eded29cbf595f59df4d9d7d5d
xiaol-luo <xiaol.luo@163.com>
2018-01-17 22:25:12 +0800

    modify

--- Code/TrySol/main.cpp
+++ Code/TrySol/main.cpp
@@ -11,23 +11,21 @@ int main(int argc, char **argv)
 	sol::state lua;
 	lua.open_libraries(sol::lib::base);
 
-	lua.script("function f (a, b, c) print(a, b) return a, b, c end");
-	sol::function fn = lua["f"];
-
+	lua.script("function f (a, b) print(a, b) return a, b end");
+	sol::function f = lua["f"];
 	TryOwnType::Head one_head;
 	one_head.weight = 10.0f;
 	one_head.param_int = 20;
-	// fn(one_head);
+	f(one_head);
+	TryOwnType::Head another_head = f(one_head);
+	printf("another_head { %f, %d } \n", another_head.weight, another_head.param_int);
 
-	// TryOwnType::Head another_head = fn(one_head);
-	// printf("another_head { %f, %d } \n", another_head.weight, another_head.param_int);
-
+	lua.script("function g (a, b, c) print(a, b) return a, b, c end");
+	sol::function g = lua["g"];
 	TryOwnType::Human one_human;
 	one_human.name = "lxl";
 	one_human.head = one_head;
-
-	fn(one_human);
-	TryOwnType::Human anohter_human = fn(one_human);
-
+	g(one_human);
+	TryOwnType::Human anohter_human = g(one_human);
 	printf("anohter_human {%s, %f, %d } \n", anohter_human.name.c_str(), anohter_human.head.weight, anohter_human.head.param_int);
 }
