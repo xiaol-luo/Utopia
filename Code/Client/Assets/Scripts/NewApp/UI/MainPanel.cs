95bc4504a3172cf550d1e1851a045a9ca97799b0
xiaol-luo <xiaol.luo@163.com>
2018-06-22 01:23:02 +0800

    add main panel

---
+++ Code/Client/Assets/Scripts/NewApp/UI/MainPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utopia;
+
+public class MainPanel : MonoBehaviour {
+
+    [SerializeField]
+    Button connectBtn;
+
+    [SerializeField]
+    Button disconnectBtn;
+
+    [SerializeField]
+    InputField ipTxt;
+
+    [SerializeField]
+    InputField portTxt;
+
+    void Start() {
+        connectBtn.onClick.AddListener(() => {
+            NewApp.instance.netModule.gameSrv.Connect(ipTxt.text, int.Parse(portTxt.text));
+        });
+
+        disconnectBtn.onClick.AddListener(() => {
+            NewApp.instance.netModule.gameSrv.Close();
+        });
+
+    }
+}
